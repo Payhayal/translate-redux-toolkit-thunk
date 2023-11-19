50ed9ed36bce84bb5294fab067bf15fcca9bb8df
“Payhayal” <begenay@hotmail.com>
2023-11-19 14:32:42 -0500

    done

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const options = {
     method: 'GET',
     url: 'https://text-translator2.p.rapidapi.com/getLanguages',
     headers: {
-      'X-RapidAPI-Key': '86a095aa69msh75e0a5b425957f3p1d114djsnd02264d0a303',
+      'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
       'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
     }
   };
